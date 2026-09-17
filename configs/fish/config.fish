7a8c91bd8ba7b4d85326473870d2e595cf2b8de3
r3p.dev <r3p.dev@gmail.com>
2026-09-17 10:20:10 +0700

    Clean up fish config by removing unused variables
    Remove BUN_INSTALL and SSH_AUTH_SOCK environment variable settings.

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -5,7 +5,3 @@ if status is-interactive
     zoxide init fish | source
     starship init fish | source
 end
-
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
